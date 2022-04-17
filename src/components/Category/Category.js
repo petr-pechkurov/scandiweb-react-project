fe0dad9bc9c705c70fadbf86b92c295c39544693
Petr Pechkurov <petr.pechkurov@gmail.com>
2022-04-17 10:29:43 +0300

    message changed

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -4,7 +4,7 @@ import ProductCard from '../ProductCard/ProductCard';
 export default class Category extends React.Component {
   render() {
     if (!this.props?.category) {
-      return <h2>There are no products in this category, or wrong category name...</h2>
+      return <h2>There are no products in this category. Please check the category name.</h2>
     }
 
     const { products, name } = this.props.category;
